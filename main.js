f62e785afee4bdda77a8c49f73c7eb5c1e2a312d
JHM5300 <62688109+JHM5300@users.noreply.github.com>
2021-02-01 13:43:00 +0900

    js: navbar menu click

--- main.js
+++ main.js
@@ -2,12 +2,24 @@
 const navbar = document.querySelector("#navbar");
 const navbarHeight= navbar.getBoundingClientRect().height;
 
+//메뉴를 스크롤하면 투명하게 만듬.
 document.addEventListener('scroll',()=>{
-    
-    console.log('navbarheight : '+navbarHeight);
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar-dark');
     }else{
         navbar.classList.remove('navbar-dark');
     }
 });
+
+// 메뉴 클릭하면 원하는곳으로 스크롤링
+const navbarmenu =  document.querySelector('.navbar_menu');
+navbar.addEventListener('click',(event)=>{
+      
+    const target = event.target;
+    const  link = target.dataset.link;
+    if(link == null){return;}
+
+    console.log(event.target.dataset.link); 
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+})
